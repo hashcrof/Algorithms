5e20051900306d83f09e35f249df187310581975
Harriet Ashcroft <harriet.ashcroft@gmail.com>
2020-05-23 11:11:37 -0400

    Updates Three Sum

--- three_sum.rb
+++ three_sum.rb
@@ -2,29 +2,36 @@ require 'set'
 # @param {Integer[]} nums
 # @return {Integer[][]}
 def three_sum(nums)
-    result = []
     found = Set.new
     n = nums.length - 1
     nums = nums.sort
     nums.each_with_index do |el, i|
-        seen = Hash.new{|h,k| h[k] = -1 }
-        #Two Sum
-        (i+1).upto(n) do |j|
-            val2 = nums[j]
-            complement = -el - val2
-            if seen[complement] > -1
-                triplet = [el, val2, complement].sort
-                min = triplet[0]
-                max = triplet[2]
-                unless found.include? [min, max]
-                    found << [min, max]
-                    result << triplet
-                end
-            end
-            seen[val2] = j
+        next if el == nums[i-1] && i > 0
+        triplets = two_sum(nums, i+1, n, el)
+        triplets.each do |triplet|
+            next if found.include? triplet
+            found << triplet
         end
     end
-    result
+    found.to_a
+end
+
+
+def two_sum(nums, from, to, target)
+    results = Set.new
+    seen = Hash.new{|h, k| h[k] = -1}
+
+    from.upto(to) do |j|
+        num = nums[j]
+        complement = -target - num
+        if seen[complement] > -1
+            results << [target, num, complement].sort
+        else
+            seen[num] = j
+        end
+    end
+
+    results.to_a
 end
 
 puts three_sum([-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6]) == [[-4,2,2],[-4,1,3],[-4,0,4],[-4,-2,6],[-2,0,2],[-2,-2,4]]
